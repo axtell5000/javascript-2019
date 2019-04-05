fd58928618d51e26c1ee412932e325e41fbf9d6c
Stephen Axtell <axtell.stephen@gmail.com>
2019-04-05 18:09:38 +0200

    some basic array work

---
+++ arrays/notes.js
@@ -0,0 +1,4 @@
+const notes = ['Note 1', 'Npte 2', 'Note 3'];
+
+console.log(notes);
+console.log(notes.length);
---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['read', 'cook dinner', 'break dance', 'keeping it real', 'sleeping'];
+
+console.log(`My todo list is ${todos.length} items long`);
+
+console.log(`The first item is: "${todos[0]}". The second last item is: "${todos[todos.length-2]}"`);
