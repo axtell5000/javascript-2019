aaf94e1c10c148305ac18978aefb466ed677d580
Stephen Axtell <axtell.stephen@gmail.com>
2019-04-06 12:35:07 +0200

    working with some basic array methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,15 @@
-const notes = ['Note 1', 'Npte 2', 'Note 3'];
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+
+// console.log(notes.pop()); // does return the item being removed from array - from the end
+// console.log(notes.shift()); // does return the item being removed from array - from the beginning
+//
+// notes.push('New note'); // add at the end
+// notes.unshift('My first note'); // add at the beginning
+
+
+//notes.splice(1,1); // remove item from index 1, second argument is how many
+notes.splice(2,0, 'New third Item'); // This will insert a new item at index 2 position
+notes.splice(1,1, 'This is new item at index 1'); // this replaces item at index 1
 
 console.log(notes);
 console.log(notes.length);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,11 @@
 const todos = ['read', 'cook dinner', 'break dance', 'keeping it real', 'sleeping'];
 
-console.log(`My todo list is ${todos.length} items long`);
+todos.splice(2, 1);
+todos.push('running');
+todos.shift();
 
-console.log(`The first item is: "${todos[0]}". The second last item is: "${todos[todos.length-2]}"`);
+// console.log(`My todo list is ${todos.length} items long`);
+//
+// console.log(`The first item is: "${todos[0]}". The second last item is: "${todos[todos.length-2]}"`);
+
+console.log(todos);
