072415b7a03f6109c2d0bbc6275d8ec4bf336305
Stephen Axtell <axtell.stephen@gmail.com>
2019-04-23 11:20:29 +0200

    extending the Person class

--- oop/person.js
+++ oop/person.js
@@ -1,4 +1,5 @@
 class Person {
+
   constructor(firstName, lastName, age, likes = []) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -23,8 +24,53 @@ class Person {
   }
 }
 
-const myPerson = new Person('Stephen', 'Axtell', 44, ['Break dancing', 'keeping it real']);
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes = []) {
+    super(firstName, lastName, age, likes);
+    this.position = position;
+  }
+
+  // Creating unique getBio() for Emoloyee
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}.`
+  }
+
+  getYearsLeft() {
+    return 65 - this.age;
+  }
+
+}
+
+class Student extends Person {
+  constructor(firstName, lastName, age, grade) {
+    super(firstName, lastName, age);
+    this.grade = grade;
+  }
+
+  getBio() {
+    if (this.grade < 70) {
+      return `${this.firstName} scored ${this.grade} and is a Fail`
+    } else {
+      return `${this.firstName} scored ${this.grade} and is a Pass`
+    }
+  }
+
+  updateGrade(adjustment) {
+    this.grade = this.grade + adjustment;
+  }
+}
+
+// const myPerson = new Person('Stephen', 'Axtell', 44, ['Break dancing', 'keeping it real']);
+// console.log(myPerson.getBio());
+
+const myPerson = new Employee('Stephen', 'Axtell', 44, 'Rocket mechanic', ['Break dancing', 'keeping it real']);
 console.log(myPerson.getBio());
+console.log(myPerson.getYearsLeft());
+
+const myStudent = new Student('Stephen', 'Axtell', 44, 70);
+console.log(myStudent.getBio());
+myStudent.updateGrade(-10);
+console.log(myStudent.getBio());
 
 
 
