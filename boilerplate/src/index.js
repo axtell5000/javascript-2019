535ef999ef222be84ff0c4205784040453c1c9ab
Stephen Axtell <axtell.stephen@gmail.com>
2019-04-27 13:23:37 +0200

    reverting to boilerplate state basivs

--- boilerplate/src/index.js
+++
@@ -1,8 +0,0 @@
-// import './utilities';
-import square, { add } from './utilities';
-import scream from './scream';
-
-console.log('index.js');
-console.log(add(10,20));
-console.log(scream('yo yo yo ze'));
-console.log(square(10));
--- boilerplate/src/utilities.js
+++
@@ -1,9 +0,0 @@
-
-export const add = (a, b) => a + b;
-
-const square = (x) => x * x;
-console.log('from my code');
-
-export default square;
-
-
