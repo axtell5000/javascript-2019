56386207c70b6346cf060e002d44e77f40effb3c
Stephen Axtell <axtell.stephen@gmail.com>
2019-04-27 17:07:55 +0200

    some project restructuring and some styling

---
+++ hangman/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  // relative
+  entry: './src/index.js',
+  output: {
+    // absolute so we have to do something different
+    path: path.resolve(__dirname, 'public/scripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/scripts/'
+  },
+  devtool: 'source-map' // for source maps allows slightly easier debugging
+};
